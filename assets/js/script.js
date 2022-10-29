62d65e02acfc02fa53bd31e353c41d144b9b9dda
mmm <margaret.m.matson@gmail.com>
2022-10-29 13:38:56 -0500

    added function and loop to update colors for time block based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
 //Display current day, date, and time using moment.js
 $('#currentDay').text(moment().format('llll'));
+
+//Color code timeblocks based on current time
+function changeColor() {
+    var currentTime = moment().hours();
+    //Execute function for each element with time block class
+    $(".time-block").each(function loopTimeBlocks () {
+        //Remove "time" string from id associated with time-block class elements and convert into an integer
+        var timeBlock = parseInt($(this).attr('id').replace("time", ''));
+        //Add and/or remove classes from time-block based on current time
+        if (timeBlock < currentTime) {
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+            $(this).addClass('past');
+        }
+        if (timeBlock === currentTime) {
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+            $(this).addClass('present');
+        }
+        if (timeBlock > currentTime) {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })
+}
+
+//Call change color function
+changeColor();
+
