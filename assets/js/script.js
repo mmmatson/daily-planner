3b73e38817213085afda08aba257fef723f7fbc3
mmm <margaret.m.matson@gmail.com>
2022-10-30 14:24:37 -0500

    created functions to set key value pairs and save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+//Define function to set key value pairs for local storage
+function saveEvent() {
+    //Children elements of each time-block assigned as key value pairs
+    $('.time-block').each(function () {
+        var time = $(this).children(".hour").text();
+        var activity = $(this).children(".description").val();
+        localStorage.setItem(time, activity);
+    })
+}
+
 //Display current day, date, and time using moment.js
 $('#currentDay').text(moment().format('llll'));
 
@@ -5,7 +15,7 @@ $('#currentDay').text(moment().format('llll'));
 function changeColor() {
     var currentTime = moment().hours();
     //Execute function for each element with time block class
-    $(".time-block").each(function loopTimeBlocks () {
+    $(".time-block").each(function loopTimeBlocks() {
         //Remove "time" string from id associated with time-block class elements and convert into an integer
         var timeBlock = parseInt($(this).attr('id').replace("time", ''));
         //Add and/or remove classes from time-block based on current time
@@ -30,3 +40,7 @@ function changeColor() {
 //Call change color function
 changeColor();
 
+//Clicking save button saves events to local storage
+$('.saveBtn').click(function () {
+    saveEvent();
+})
